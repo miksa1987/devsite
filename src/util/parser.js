18719041b18c3e0a8bd421b106e194992dbe9f67
Mika Laaksonen <mika.laaksonen@mehilainen.fi>
2021-08-19 22:10:16 +0300

    fix: blog content rendering

--- src/util/parser.js
+++ src/util/parser.js
@@ -7,6 +7,7 @@ import {
   Body,
   Code,
   FullWidth,
+  CodeFullWidth,
 } from "../components/general";
 
 const textContentTypes = [
@@ -18,86 +19,146 @@ const textContentTypes = [
   "heading-5",
 ];
 
-export const parseContent = (content, assets) =>
-  content.map((chunk, index) => {
-    const chunkTextContent = textContentTypes.includes(chunk.nodeType)
-      ? chunk.content[0].value
-      : "";
+const renderText = (chunk) => {
+  const textContent = chunk.value;
+  const styles = chunk.marks.map((mark) => mark.type);
 
-    if (chunkTextContent === "") {
-      return null;
-    }
+  if (styles.includes("code")) {
+    return (
+      <CodeFullWidth>
+        <Code>{textContent}</Code>
+      </CodeFullWidth>
+    );
+  }
+
+  const isBold = styles.includes("bold");
+  const isItalic = styles.includes("italic");
+  const isUnderline = styles.includes("underline");
+
+  return (
+    <FullWidth>
+      <Body bold={isBold} italic={isItalic} underline={isUnderline}>
+        {textContent}
+      </Body>
+    </FullWidth>
+  );
+};
+
+const renderHyperlink = (chunk) => {
+  const uri = chunk.data.uri;
+  const text = chunk.content[0].value; // TODO checkcheck
+
+  return (
+    <FullWidth>
+      <a href={uri}>{text}</a>
+    </FullWidth>
+  );
+};
+
+const renderEmbeddedBlock = (chunk, assets) => {
+  if (
+    chunk.data?.target?.sys?.type === "Link" &&
+    chunk.data?.target?.sys?.linkType === "Asset"
+  ) {
+    const asset = assets.find((asset) => asset.id === chunk.data.target.sys.id);
+    return <Image {...asset} />;
+  }
+  return null;
+};
+
+const renderParagraph = (content) => {
+  const paragraphFunctions = {
+    text: renderText,
+    hyperlink: renderHyperlink,
+  };
+  const chunkContent = content.map((innerChunk, index) => {
+    const fn = paragraphFunctions[innerChunk.nodeType];
+
+    return (
+      <React.Fragment key={index}>{fn ? fn(innerChunk) : null}</React.Fragment>
+    );
+  });
+
+  return chunkContent;
+};
+
+const renderHeading = (Component) => (chunk) => {
+  const textContent = chunk.content[0].value; // TODO check if needed to loop
+
+  return (
+    <FullWidth>
+      <Component>{textContent}</Component>
+    </FullWidth>
+  );
+};
+
+const contentFunctions = {
+  paragraph: renderParagraph,
+  "heading-1": renderHeading(Title),
+  "heading-2": renderHeading(SubTitle),
+  "heading-3": renderHeading(({ children }) => (
+    <Body bold as="h4">
+      {children}
+    </Body>
+  )),
+  "heading-4": renderHeading(({ children }) => (
+    <Body bold as="h5">
+      {children}
+    </Body>
+  )),
+  "embedded-asset-block": renderEmbeddedBlock,
+};
+
+export const aparseContent = (content, assets) =>
+  content?.map((chunk, index) => {
+    const fn = contentFunctions[chunk.nodeType];
+    return (
+      <React.Fragment key={index}>
+        {fn ? fn(chunk, assets) : null}
+      </React.Fragment>
+    );
+  });
+
+export const parseContent = (content, assets) =>
+  content.map((chunk) => {
+    let Component;
 
     switch (chunk.nodeType) {
       case "paragraph":
-        const styles = chunk.content[0].marks.map((mark) => mark.type);
-
-        if (styles.includes("code")) {
-          return <Code>{chunkTextContent}</Code>;
-        }
-
-        const isBold = styles.includes("bold");
-        const isItalic = styles.includes("italic");
-        const isUnderline = styles.includes("underline");
-
-        return (
-          <FullWidth key={index}>
-            <Body bold={isBold} italic={isItalic} underline={isUnderline}>
-              {chunkTextContent}
-            </Body>
-          </FullWidth>
-        );
+        return renderParagraph(chunk.content);
 
       case "heading-1":
-        return (
-          <FullWidth key={index}>
-            <Title>{chunkTextContent}</Title>
-          </FullWidth>
-        );
+        return renderHeading(Title)(chunk);
 
       case "heading-2":
-        return (
-          <FullWidth key={index}>
-            <SubTitle>{chunkTextContent}</SubTitle>
-          </FullWidth>
-        );
+        return renderHeading(SubTitle)(chunk);
 
       case "heading-3":
-        return (
-          <FullWidth key={index}>
-            <SubSubTitle>{chunkTextContent}</SubSubTitle>
-          </FullWidth>
+        Component = ({ children }) => (
+          <SubTitle bold as="h3">
+            {children}
+          </SubTitle>
         );
+        return renderHeading(Component)(chunk);
 
       case "heading-4":
-        return (
-          <FullWidth key={index}>
-            <Body bold as="h4">
-              {chunkTextContent}
-            </Body>
-          </FullWidth>
+        Component = ({ children }) => (
+          <Body bold as="h4">
+            {children}
+          </Body>
         );
+        return renderHeading(Component)(chunk);
 
       case "heading-5":
-        return (
-          <FullWidth key={index}>
-            <Body bold as="h5">
-              {chunkTextContent}
-            </Body>
-          </FullWidth>
+        Component = ({ children }) => (
+          <Body bold as="h5">
+            {children}
+          </Body>
         );
+        return renderHeading(Component)(chunk);
 
       case "embedded-asset-block":
-        if (
-          chunk.data?.target?.sys?.type === "Link" &&
-          chunk.data?.target?.sys?.linkType === "Asset"
-        ) {
-          const asset = assets.find(
-            (asset) => asset.id === chunk.data.target.sys.id
-          );
-          return <Image key={index} {...asset} />;
-        }
-        return null;
+        return renderEmbeddedBlock(chunk, assets);
 
       default:
         return null;
